f9d622d2ba50d50bb05c28d3d7b32fe28a5f1259
Rhytah <ritanamono@gmail.com>
2018-08-01 14:51:10 +0300

    Writing tests -data

---
+++ tests/__init__.py
@@ -0,0 +1,31 @@
+from tracker.mtracker import app
+import unittest
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        self.test_client = app.test_client()
+        self.user_data = {
+            "user_id": 1,
+            "name": "Rita",
+            "email":"rita2@hotmail.com",
+            "userName": "Rhytah",
+            "password": "thisisandela"
+        }
+        self.user_login_data={
+            "userName":"Rhytah",
+            "password":"thisisandela"
+        }
+        self.request_data={
+            "requestId":1,
+            "employeeName":"Seth",
+            "description":"finance",
+            "category":"Computer-Peripheral",
+            "requestDate":"12/07/2018",
+            "title": "Replace Mouse",
+        }
+    def tearDown(self):
+        requests = []
+        users = []
+
+if __name__ == "__main__":
+    unittest.main()
