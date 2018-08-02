1eb4d2ea4304a77c04aa12f3c68ed743c7d36d32
Rhytah <ritanamono@gmail.com>
2018-08-02 04:11:08 +0300

    test request file

---
+++ tests/test_requests.py
@@ -0,0 +1,22 @@
+from tests import BaseTestCase
+import json
+
+class RequestTestCase(BaseTestCase):
+
+    def test_create_request(self):
+        response = self.test_client.post(
+            '/api/v1/users/requests', data=json.dumps(self.request_data),content_type='application/json'
+        )
+        self.assertEqual(response.status_code,200)
+        self.assertIn(
+            "Hello Seth! Request succesfully created", str(response.data)
+        )
+    def test_get_all_requests(self):
+        response = self.test_client.post(
+            '/api/v1/users/requests', data =json.dumps(self.request_data),content_type='application/json')
+        self.assertEqual(response.status_,200)
+    
+    def test_get_single_request(self):
+        response = self.test_client.get(
+            '/api/v1/users/requests/1', data=json.dumps(self.request_data), content_type='application/json')
+        self.assertEqual(response.status_code,200)
