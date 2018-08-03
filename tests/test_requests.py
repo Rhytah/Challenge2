78fb9a95cad5943056c0d2ffbc83ea89d7d23434
Rhytah <ritanamono@gmail.com>
2018-08-04 01:59:53 +0300

    bug fix

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -14,7 +14,7 @@ class RequestTestCase(BaseTestCase):
     def test_get_all_requests(self):
         response = self.test_client.post(
             '/api/v1/users/requests', data =json.dumps(self.request_data),content_type='application/json')
-        self.assertEqual(response.status_,200)
+        self.assertEqual(response.status_code,200)
     
     def test_get_single_request(self):
         response = self.test_client.get(
