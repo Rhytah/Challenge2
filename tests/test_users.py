59ca5330e03e89c36e94a1a95639188eb75a5b4d
Rhytah <ritanamono@gmail.com>
2018-08-07 04:34:05 +0300

    testing a user registration

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,4 +3,8 @@ import json
 
 class UserTestCase(BaseTestCase):
     def test_register_user(self):
-        response = self.test_client.post()
+        response = self.test_client.post(
+            '/auth/register', data=json.dumps(self.user_data), content_type= 'application/json'
+        )
+        self.assertEqual(response.status_code,200)
+        self.assertIn("User Rhytah has successfully been created", str(response.data))
